9f57124b6c353572d67acbc93e0cd3f886550919
Kia Choi <sgchoi529@email.arizona.edu>
2018-01-11 13:49:08 -0700

    Completed App.js

--- Mobile-II/App.js
+++ Mobile-II/App.js
@@ -1,21 +1,41 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, Button } from 'react-native';
+import { StackNavigator } from "react-navigator";
 
-export default class App extends React.Component {
+import TodoList from './TodoList';
+import SignUp from './SignUp';
+import SignIn from './SignIn';
+import Content from './Content';
+import styles from './Styles';
+
+class Home extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style = {styles.container}>
+        <Text style={styles.header}>Welcome to your Todo List! Please Sign In.</Text>
+        <Button
+          title = "Sign In"
+          onPress = {() => {
+            this.props.navigation.navigate('SignIn');
+          }}
+        />
+        <Button
+          title = "Sign Up"
+          onPress = {() => {
+            this.props.navigation.navigate('SignUp');
+          }}
+        />
+
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const Routes = StackNavigator ({
+  Home: { screen: Home },
+  TodoList: { screen: TodoList },
+  SignIn: { screen: SignIn },
+  SignUp: { screen: SignUp }
 });
+
+export default Routes;
