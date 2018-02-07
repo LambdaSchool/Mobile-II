8a34bf3d905d17773d324a7edd9f7f61aeed3422
atiffany <abby.tiffany@gmail.com>
2018-02-07 15:50:44 -0800

    errors trying to incorporate printing out list of users in content component

--- todoapp/Content.js
+++ todoapp/Content.js
@@ -3,20 +3,54 @@ import {
     View,
     Text,
     TextInput,
-    Button
+    Button,
+    Flatlist
 } from 'react-native';
+
+const ROOT_URL = 'https://mobile-server-ii.herokuapp.com';
 
 export default class Content extends React.Component {
     constructor() {
         super();
+        this.state = {
+            users : [],
+        };
     }
 
-    componentDidMount() {};
+    componentDidMount() {
+        AsyncStorage.getItem('token')
+            .then((token) => {
+                axios
+                    .get(`${ROOT_URL}/users`, {
+                        headers: {
+                            authorization: token,
+                        }
+                    })
+                    .then(res => {
+                        if (res !== null) {
+                            this.setState({ users: res });
+                        }
+                    })
+            })
+            .catch(error => {
+                console.log('Error getting your token: ', error);
+            });
+    };
 
     render () {
         return (
             <View>
-                <Text>Sup B</Text>
+                <Text>List of Users of this Service</Text>
+                <Flatlist
+                    data={this.state.users}
+                    renderItem={({item, index}) => {
+                        return (
+                            <View key = {index}>
+                                <Text>{item.username}</Text>
+                            </View>
+                        );
+                    }}
+                />
             </View>
         );
     }
