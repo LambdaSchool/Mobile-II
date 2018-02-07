ad2009e1d7e06dbd606e2a86619f39e12d6b00c9
atiffany <abby.tiffany@gmail.com>
2018-02-07 13:01:34 -0800

    trying to add axios request and receive jwt to no avail

--- todoapp/SignUp.js
+++ todoapp/SignUp.js
@@ -3,15 +3,18 @@ import {
     View,
     Text,
     TextInput,
-    Button
+    Button,
+    AsyncStorage
 } from 'react-native';
 import axios from 'axios';
+
+const ROOT_URL = 'https://mobile-server-ii.herokuapp.com/';
 
 export default class SignUp extends React.Component {
     constructor() {
         super();
         this.state = {
-            username: '',
+            email: '',
             password: '',
         };
     }
@@ -19,17 +22,33 @@ export default class SignUp extends React.Component {
 
     handleFormSubmit(event) {
         event.preventDefault();
+        const email = this.state.email;
+        const password = this.state.password;
         axios
-            .post('/users', {})
-    }
+            .post(`${ROOT_URL}/users`, { email, password })
+            .then(jwt => {
+                AsyncStorage.setItem('authorization', JSON.stringify(jwt), error => {
+                    if(error) {
+                        console.log('Error saving JWT: ', error);
+                    } else {
+                        console.log('yeah BB');
+                    }
+                });
+
+            })
+            .catch(error => {
+                console.log('Error saving User to DB: ', error);
+            });
+
+    };
 
     render () {
         return (
             <View>
                 <Text>Sign Up Screen</Text>
-                <TextInput onChangeText={(text) => this.setState({ username: text}) } placeholder = 'Enter Your Email Address' />
+                <TextInput onChangeText={(text) => this.setState({ email: text}) } placeholder = 'Enter Your Email Address' />
                 <TextInput onChangeText={(text) => this.setState({ password: text }) } placeholder = 'Enter Your Password' />
-                <Button onPress={() =>  console.log('booya')} title='Create Account' />
+                <Button onPress={(event) => this.handleFormSubmit} title='Create Account' />
             </View>
         );
     }
